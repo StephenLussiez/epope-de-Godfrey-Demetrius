36ca357c706c778aad7d4ccce4775ff0c6175d8e
YFarant22 <yann.farant@ynov.com>
2023-04-03 12:46:20 +0200

    Updating WindowManager

--- epope-de-Godfrey-Demetrius/Include/managers/WindowManager.h
+++ epope-de-Godfrey-Demetrius/Include/managers/WindowManager.h
@@ -1,13 +1,14 @@
 #include <SFML/Graphics.hpp>
 
 class WindowManager {
-	WindowManager* instance;
+	static WindowManager* m_instance;
 	int m_width;
 	int m_height;
 	std::string m_name;
 	sf::RenderWindow* m_window;
 
 public:
+	static WindowManager* GetInstance();
 	WindowManager();
 	sf::RenderWindow* CreateWindow(int width, int height, std::string name);
 	sf::RenderWindow* GetWindow();
